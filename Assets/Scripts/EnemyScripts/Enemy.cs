a299c4dfea74a4763e1f2407245990467184121c
Jordopol <jordi.tur@estudiantat.upc.edu>
2024-11-30 23:28:44 +0100

    Implemented Enemy Base Class and some enemy scripts

---
+++ Assets/Scripts/EnemyScripts/Enemy.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Enemy : MonoBehaviour
+{
+    public float detectionRange = 5f;
+    public float moveSpeed = 2f;
+    protected Transform player;
+
+    protected virtual void Start()
+    {
+        player = GameObject.FindWithTag("Player").transform;
+    }
+
+    public abstract void Patrol();
+
+    protected virtual void Update()
+    {
+        Patrol();
+    }
+
+    public abstract void OnPlayerDetected();
+
+    public virtual void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    protected bool IsPlayerInRange()
+    {
+        return Vector2.Distance(transform.position, player.position) <= detectionRange;
+    }
+
+}
+
+
